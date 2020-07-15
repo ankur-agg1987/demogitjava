91dd3408a55e6dc800b1b1cbd03d01bf2ca1d370
ankur-agg1987 <40783014+ankur-agg1987@users.noreply.github.com>
2020-07-15 13:27:56 +0530

    second commit

--- src/demogitprojectjava/DemoGitProjectJava.java
+++ src/demogitprojectjava/DemoGitProjectJava.java
@@ -8,6 +8,10 @@ public class DemoGitProjectJava {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        
+        int a =5;
+        
+        System.out.print(a);
     }
     
 }
