cef7786b9d8e1d101773996d5619d5c00a08a677
ankur-agg1987 <40783014+ankur-agg1987@users.noreply.github.com>
2020-07-15 13:23:51 +0530

    first commit

---
+++ src/demogitprojectjava/DemoGitProjectJava.java
@@ -0,0 +1,13 @@
+package demogitprojectjava;
+
+/**
+ *
+ * @author Ankur
+ */
+public class DemoGitProjectJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
